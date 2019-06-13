f9af396ca7a599cc353e07f0e1119af27c9adc70
Beatriz <beatriz.tonella@gmail.com>
2019-06-13 11:26:17 -0300

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 
+
+mongoose.connect('mongodb+srv://user:senha@cluster0-z8ksc.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+})
 
 app.get("/", (req, res) => {
     return res.send("Hello "+ req.query.name)
