e3783f24911190d99cfdf16328cb0ea0003cd3ae
Beatriz <beatriz.tonella@gmail.com>
2019-06-13 12:00:31 -0300

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const config = require("../config")
 
 const app = express()
 
-
-mongoose.connect('mongodb+srv://user:senha@cluster0-z8ksc.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${config.user}:${config.password}@cluster0-z8ksc.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
 })
 
-app.get("/", (req, res) => {
-    return res.send("Hello "+ req.query.name)
-})
+app.use(require("./routes"))
 app.listen(3333)
